fce95a8b0dd0bdb975dc0f5c58584f5434c6f815
João Pedro Lopes <lopes_230202@outlook.com>
2023-09-06 11:53:36 -0300

    Add files via upload

--- rato.py
+++ rato.py
@@ -7,42 +7,44 @@ third = Rational(1, 3)
 
 Tfrac = Matrix(
     [
-        [1, half, 0, half, 0],
-        [half, 1, third, 0, 0],
-        [0, half, 1, half, 0],
-        [half, 0, third, 1, 0],
+        [0, half, 0, half, 0],
+        [half, 0, third, 0, 0],
+        [0, half, 0, half, 0],
+        [half, 0, third, 0, 0],
         [0, 0, third, 0, 1],
     ]
 )
 
 T = Matrix(
     [
-        [1, 0.5, 0, 0.5, 0],
-        [0.5, 1, 0.33, 0, 0],
-        [0, 0.5, 1, 0.5, 0],
-        [0.5, 0, 0.33, 1, 0],
+        [0, 0.5, 0, 0.5, 0],
+        [0.5, 0, 0.33, 0, 0],
+        [0, 0.5, 0, 0.5, 0],
+        [0.5, 0, 0.33, 0, 0],
         [0, 0, 0.33, 0, 1],
     ]
 )
 
 
-def prob_teste(j):
+def prob_teste():
     # Fórmula -> (P ** k) * P.col(j)
     y = []
-    for k in range(1, 11):
-        a = (T**k) * T.col(j)
+    for j in range(0, 4):
+        ej = Matrix([[0], [0], [0], [0], [0]])
+        ej[j] = 1
+        a = (T**10) * ej
 
         y.append(a.evalf(2))
 
-        if a[4, 0] > 1:
-            print(f"    Coluna: {j + 1} : k = {k} \n")
-            break
+        print(f"Testanto coluna {j + 1} \n")
+        pprint([a.evalf(2), ej])
 
 
 def infinite_Diagonal(matrix):
-    for i in range(1, 1000000, 10000):
-        print(f"Matriz diagonal elevada a {i}: \n")
-        pprint((matrix**i).evalf(2))
+    for k in range(1, 100, 10):
+        print(f"Limite de k -> +oo | k = {k}: \n")
+        pprint((matrix**k).evalf(2))
+        pprint(limit(matrix, k, oo, "+"))
         print("\n")
 
 
@@ -52,17 +54,16 @@ def main():
     pprint(Tfrac)
     print("\n")
 
-    for j in range(0, 4):
-        print(f"Testanto coluna {j + 1} \n")
-        prob_teste(j)
+    prob_teste()
 
     M, D = T.diagonalize()
     Diag = (D).evalf(2)
 
     print("\n T diagonalizada: \n")
-    pprint(Diag)
+    pprint(Tfrac)
+    print("\n")
 
-    infinite_Diagonal(Diag)
+    infinite_Diagonal(T)
 
     if input():
         exit()
